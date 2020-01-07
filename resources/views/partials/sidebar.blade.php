85d42f89c02e8ce577a2b783daffc596c9c30a2e
Joseph <joraff@gmail.com>
2020-01-07 17:29:25 -0600

    Fixes sidebar html typo

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,5 +1,5 @@
 @if( $has_sidebar )
-<aside class="sidebar" data-has-sidebay="{!!  $has_sidebar !!}">
+<aside class="sidebar" data-has-sidebar="{!!  $has_sidebar !!}">
   <!-- <button class="toggle collapsed" type="button" data-toggle="collapse" data-target="#sidebar-nav" aria-controls="sidebar-nav" aria-expanded="false" aria-label="Toggle docs navigation">Page Menu</button> -->
   <nav aria="Sidebar Menu" class="" id="sidebar-nav">
     {!! $sidebar_content !!}
