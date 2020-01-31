515823937af13ee1c18610d0343e33d967102efc
Joseph <joraff@gmail.com>
2020-01-31 14:09:21 -0600

    Suppress warning when no sidebar

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,4 +1,4 @@
-@if( $has_sidebar )
+@if( @$has_sidebar )
 <aside class="sidebar" data-has-sidebar="{!!  $has_sidebar !!}">
   <!-- <button class="toggle collapsed" type="button" data-toggle="collapse" data-target="#sidebar-nav" aria-controls="sidebar-nav" aria-expanded="false" aria-label="Toggle docs navigation">Page Menu</button> -->
   <nav aria="Sidebar Menu" class="" id="sidebar-nav">
