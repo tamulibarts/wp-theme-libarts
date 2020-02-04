facc596c63e67975217fef8d99182a076daa403b
Joseph <joraff@gmail.com>
2020-02-04 09:45:55 -0600

    Delegates directory group rendering to a block

--- resources/views/partials/profile_group.blade.php
+++ resources/views/partials/profile_group.blade.php
@@ -1,7 +1,9 @@
-<div class="directory-group">
-  @foreach( $profile_group['profiles'] as $profile )
-    @include('partials.profile_group-profile', ['profile' => $profile])
+@php
+  $blocks = parse_blocks('<!-- wp:acf/group { "name": "acf\/group", "data": { "group": 6132, "_group": "group_post" }} /-->');
+@endphp
 
-
+@if( $blocks )
+  @foreach( $blocks as $block )
+    {!! render_block( $block ) !!}
   @endforeach
-</div>
+@endif
