e7112ff92cad1a6e840ed780f2df3ac13255fce4
Joseph <joraff@gmail.com>
2020-02-05 10:06:40 -0600

    Adds support for no-link breadcrumb

--- resources/views/partials/breadcrumbs.blade.php
+++ resources/views/partials/breadcrumbs.blade.php
@@ -3,7 +3,11 @@
     <li><a href="{{$homeurl}}">Home</a></li>
     @foreach($breadcrumbs as $breadcrumb)
     <li>
+    @if($breadcrumb->link)
       <a href="{{$breadcrumb->link}}" {{ $breadcrumb->current ?: 'aria-current="true"' }}>{{$breadcrumb->title}}</a>
+    @else
+      {{$breadcrumb->title}}
+    @endif
     </li>
     @endforeach
   </ol>
